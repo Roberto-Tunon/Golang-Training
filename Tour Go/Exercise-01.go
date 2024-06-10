86e248b469a4af7d4926ecad5c4fbcb2909f1973
robe <ROBE@xxxlgroup.com>
2024-06-10 15:16:57 +0200

    First commit

---
+++ Tour Go/Exercise-01.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func add(x , y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return x,y
+}
+
+func main() {	
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
+	fmt.Println("My favorite number is", rand.Intn(10))
+
+	fmt.Println(add(42, 13))
+	/*
+	  Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+      Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
+	*/  
+	var i, j int = 1, 2
+    k := 3
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+
+	fmt.Println(split(17))
+}
---
+++ Tour Go/Intro.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main()  {
+   var a,b int
+   a = 42
+   b = 7
+   fmt.Println("a vale:", a)
+   fmt.Printf("a as binary: %b \n", a)
+   fmt.Printf("b as hexadecimal: %#X \n\n", b)
+
+   // print these values as both binary & hexadecimal
+	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+   fmt.Printf("Value \t Binary\t Hexadecimal\n")
+   fmt.Printf("----- \t ------\t -----------\n")
+   fmt.Printf("%v \t %b \t %#X\n", a,a,a)
+   fmt.Printf("%v \t %b \t %#X\n", b,b,b)
+   fmt.Printf("%v \t %b \t %#X\n", c,c,c)
+   fmt.Printf("%v \t %b \t %#X\n", d,d,d)
+   fmt.Printf("%v \t %b \t %#X\n", e,e,e)
+   fmt.Printf("%v \t %b \t %#X\n", f,f,f)
+
+   y := 42
+   z := 42.0
+   fmt.Printf("%v of type %T \n", y, y)
+   fmt.Printf("%v of type %T \n", z, z)
+
+   var m float32 = 43.742
+   fmt.Printf("%v of type %T \n", m, m)
+
+   /*
+		// this does not work!
+		// in go you can't take a VALUE that is float32 and store it 
+		// in a variable that is declared to hold a VALUE of float64
+		z = m 
+		fmt.Printf("%v of type %T \n", z, z)
+   */
+
+   // this does work
+   z = float64(m)
+   fmt.Printf("%v of type %T \n", z, z)
+	
+   
+	
+}
