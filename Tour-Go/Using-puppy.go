35c04d885f0a82b36cc3b31fd2f53e7f7a9cccae
robe <ROBE@xxxlgroup.com>
2024-06-16 11:14:17 +0200

    Third

---
+++ Tour-Go/Using-puppy.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/Roberto-Tunon/GO-Puppy"
+)
+
+func main() {
+	s1 := GO-Puppy.Bark()
+	s2 := GO-Puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// also
+	fmt.Println(GO-Puppy.Bark())
+	fmt.Println(GO-Puppy.Barks())
+}
