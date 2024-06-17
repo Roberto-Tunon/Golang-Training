e7492ab9bf976b3fdfa79455125f8e130c58c721
robe <ROBE@xxxlgroup.com>
2024-06-17 09:48:30 +0200

    4

--- Tour-Go/Exercise-01.go
+++ Tour-Go/Exercise-01.go
@@ -6,7 +6,7 @@ import (
 	"math/rand"
 )
 
-func add(x , y int) int {
+func add(x, y int) int {
 	return x + y
 }
 
@@ -17,21 +17,21 @@ func swap(x, y string) (string, string) {
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
-	return x,y
+	return x, y
 }
 
-func main() {	
+func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 	fmt.Println(math.Pi)
 	fmt.Println("My favorite number is", rand.Intn(10))
 
 	fmt.Println(add(42, 13))
 	/*
-	  Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
-      Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
-	*/  
+			  Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+		      Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
+	*/
 	var i, j int = 1, 2
-    k := 3
+	k := 3
 
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
