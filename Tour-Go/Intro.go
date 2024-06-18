0a1a8c9e05ae4600d19b6b035ebc1670f6424590
robe <ROBE@xxxlgroup.com>
2024-06-18 16:36:51 +0200

    Flow

--- Tour-Go/Intro.go
+++ Tour-Go/Intro.go
@@ -20,7 +20,7 @@ func main3()  {
    fmt.Printf("%v \t %b \t %#X\n", d,d,d)
    fmt.Printf("%v \t %b \t %#X\n", e,e,e)
    fmt.Printf("%v \t %b \t %#X\n", f,f,f)
-
+   
    y := 42
    z := 42.0
    fmt.Printf("%v of type %T \n", y, y)
